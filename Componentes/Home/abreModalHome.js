5d81eb7e3daf3acbb5310fa1df22ffa8a8e9e3d9
Diogo <oliveiradiogo50@gmail.com>
2023-06-23 11:06:55 -0300

    Adicionando modal

---
+++ Componentes/Home/abreModalHome.js
@@ -0,0 +1,35 @@
+const modal1 = document.querySelector('#modal-1');
+
+function abreModal1(){
+    modal1.style.opacity = ` 1 `
+    modal1.style.display = ` flex `
+}
+
+function fechaModal1(){
+    modal1.style.opacity = ` 0 `
+    modal1.style.display = ` none `
+}
+
+const modal2 = document.querySelector('#modal-2');
+
+function abreModal2(){
+    modal2.style.opacity = ` 1 `
+    modal2.style.display = ` flex `
+}
+
+function fechaModal2(){
+    modal2.style.opacity = ` 0 `
+    modal2.style.display = ` none `
+}
+
+const modal3 = document.querySelector('#modal-3');
+
+function abreModal3(){
+    modal3.style.opacity = ` 1 `
+    modal3.style.display = ` flex `
+}
+
+function fechaModal3(){
+    modal3.style.opacity = ` 0 `
+    modal3.style.display = ` none `
+}
--- Componentes/Home/navFixed.js
+++ Componentes/Home/navFixed.js
@@ -9,14 +9,6 @@ window.onscroll = function() {
     } else{
       document.getElementById("navbar").style.backgroundColor = ` #000000 `
     }
-    if (prevScrollpos >= currentScrollPos) {
-      document.getElementById("navbar").style.position = ` fixed `
-      document.getElementById("navbar").style.top = "0";
-  
-    } else {
-      document.getElementById("navbar").style.position = ` fixed `
-      document.getElementById("navbar").style.top = "-8.5rem";
-    }
     prevScrollpos = currentScrollPos;
 
 }
